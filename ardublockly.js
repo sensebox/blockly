8f1b4e8d52e8b1380cb85dab7e2373d62a3654a9
felixerdy <felix.erdmann@uni-muenster.de>
2017-06-20 16:09:37 +0200

    relative path

--- ardublockly.js
+++ ardublockly.js
@@ -16,7 +16,7 @@ Ardublockly.init = function() {
 
   // Inject Blockly into content_blocks and fetch additional blocks
   Ardublockly.injectBlockly(document.getElementById('content_blocks'),
-                            Ardublockly.TOOLBOX_XML, '../blockly/');
+                            Ardublockly.TOOLBOX_XML, './blockly/');
   Ardublockly.importExtraBlocks();
 
   Ardublockly.designJsInit();
