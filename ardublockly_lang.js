b27d4b801d8a9e0b49d954d78c9e62480fbb0016
Felix Erdmann <felix.erdmann@uni-muenster.de>
2017-08-31 18:09:32 +0200

    Update ardublockly_lang.js

--- ardublockly_lang.js
+++ ardublockly_lang.js
@@ -144,7 +144,7 @@ Ardublockly.injectLanguageJsSources = function(langKey) {
 
   // Retrieve and inject Blockly translations asynchronously
   var blocklyLangJsLoad = document.createElement('script');
-  blocklyLangJsLoad.src = '../blockly/msg/js/' + langKey + '.js';
+  blocklyLangJsLoad.src = './blockly/msg/js/' + langKey + '.js';
   head.appendChild(blocklyLangJsLoad);
 };
 
